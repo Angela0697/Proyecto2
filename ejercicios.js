677396f3be2079d890c01eab54d9374203c70fb3
Angela Laverde <angelalaverde97@gmail.com>
2025-03-26 16:45:00 -0600

    Agrego archivo ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,15 @@
+//Par o Impar
+
+const prompt = require("prompt-sync")();
+
+let number = Number(prompt("Escriba un número: "));
+
+function parImpar(number) {
+    if (number % 2 == 0) {
+        return "El número es par";
+    } else {
+        return "El número es impar";
+    }
+}
+
+console.log(parImpar(number))
